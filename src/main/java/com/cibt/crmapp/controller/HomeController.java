209507a66215697f931e021db7370420afd9809b
sipaisanjay <sipai.sanjay07@gmail.com>
2019-06-07 14:07:55 +0545

    Second push

---
+++ src/main/java/com/cibt/crmapp/controller/HomeController.java
@@ -0,0 +1,25 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.cibt.crmapp.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+/**
+ *
+ * @author Sanjay07
+ */
+@Controller
+@RequestMapping("/")
+public class HomeController {
+    
+    @GetMapping
+    public String index(){
+        return "index";
+    }
+    
+}
